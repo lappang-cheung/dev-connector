5857511b7a9b58ce6ea4d9e6da8031b7e6621881
Leo Cheung <lappangcheung@gmail.com>
2018-06-24 00:25:28 -0400

    add post route done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Loading post model
+const Post = require('../../models/Post');
+
+// Loading post validation
+const validatePostInput = require('../../validation/post');
 
 /*
  * @route   GET api/posts/test
@@ -10,6 +18,31 @@ router.get('/test', (req,res) => {
     res.status(200).json({
         message: 'Connected to posts'
     })
+});
+
+/*
+ * @route   Post api/posts
+ * @desc    Crate posts route
+ * @access  Private
+ */
+router.post('/', passport.authenticate('jwt', { session: false }), (req,res) => {
+
+    const {errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if(!isValid){
+        // If any errors, send 400 with errors object
+        return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.name,
+        user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
 });
 
 module.exports = router;
