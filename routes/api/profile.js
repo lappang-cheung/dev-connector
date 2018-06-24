8717bf88b14a4a661ca95ec6b50fd78d31440dbf
Leo Cheung <lappangcheung@gmail.com>
2018-06-23 22:44:51 -0400

    update to show name and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,6 +33,7 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req,res) => {
     Profile.findOne({
         user: req.user.id
     })
+    .populate('user', ['name', 'avatar'])
     .then(profile => {
         if(!profile){
             errors.noprofile = 'There is no profile for this user';
