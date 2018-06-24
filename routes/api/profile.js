ee8422311a7c3e75c9639dbc32fd2590d21f7a84
Leo Cheung <lappangcheung@gmail.com>
2018-06-23 22:20:52 -0400

    edit or create profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -19,7 +19,7 @@ router.get('/test', (req,res) => {
 });
 
 /*
- * @route   GET api/profile/test
+ * @route   GET api/profile
  * @desc    Get current profile route
  * @access  Private
  */
@@ -38,6 +38,42 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req,res) => {
         res.json(profile);
     })
     .catch(err => res.status(404).json(err));
+    
 });
+
+/*
+ * @route   POST api/profile
+ * @desc    Create or edit current profile route
+ * @access  Private
+ */
+router.post('/', passport.authenticate('jwt', {session: false}), (req,res) => {
+
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    // Checking condition
+    if(req.body.handle) profileFields.handle = req.body.handle;
+    if(req.body.company) profileFields.company = req.body.company;
+    if(req.body.website) profileFields.website = req.body.website;
+    if(req.body.location) profileFields.location = req.body.location;
+    if(req.body.bio) profileFields.bio = req.body.bio;
+    if(req.body.status) profileFields.status = req.body.status;
+    if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+
+    // Skills - Split into array
+    if(typeof req.body.skills !== 'undefined'){
+        profileFields.skills = req.body.skills.split(',');
+    }
+
+    // Social links
+    profileFields.social = {};
+    if(req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
+});
+
 
 module.exports = router;
