76be00dcc8b230a88d24d198a47b9dfada2a5aac
Leo Cheung <lappangcheung@gmail.com>
2018-06-24 00:09:36 -0400

    remove the user profile and account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -330,5 +330,24 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', {session: false
     
  });
 
+/*
+ * @route   DELETE api/profile
+ * @desc    Delete user and profile
+ * @access  Private
+ */
+
+router.delete('/', passport.authenticate('jwt', {session: false}), (req,res) => {
+
+    Profile.findOneAndRemove({
+        user: req.user.id
+    })
+    .then(() => {
+        User.findOneAndRemove({
+            _id: req.user.id
+        })
+        .then(() => res.json({ success: true }))
+    })
+ });
+
 
 module.exports = router;
