febb6a03fbdb826be1c0d1247cacffe4e356afad
Leo Cheung <lappangcheung@gmail.com>
2018-06-23 22:42:06 -0400

    update and edit profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load Profile && User model
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -47,6 +50,14 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req,res) => {
  * @access  Private
  */
 router.post('/', passport.authenticate('jwt', {session: false}), (req,res) => {
+
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check validation
+    if(!isValid){
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
 
     // Get fields
     const profileFields = {};
