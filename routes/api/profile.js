371c5dd1de59704a176123a448dbf5fdad945b6a
Leo Cheung <lappangcheung@gmail.com>
2018-06-23 22:34:06 -0400

    validators for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -73,6 +73,43 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req,res) => {
     if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
     if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({
+        user: req.user.id
+    })
+    .then(profile => {
+        if(profile){
+            // Update profile
+            Profile.findOneAndUpdate(
+                {
+                    user: req.user.id
+                },
+                {
+                    $set: profileFields
+                },
+                {
+                    new: true
+                }           
+            )
+            .then(profile => res.json(profile));
+        } else {
+            // Create Profile
+
+            // Check if handle exists
+            Profile.findOne({
+                handle: profileFields.handle
+            })
+            .then(profile => {
+                if(profile){
+                    errors.handle = 'That handle already exists';
+                    res.status(400).json(errors);
+                }
+
+                // Save Profile
+                new Profile(profileFields).save().then(profile => res.json(profile));
+            })
+        }
+    })
 });
 
 
