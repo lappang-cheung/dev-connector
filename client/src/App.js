1b2f70ea08b81455000b082808e01ecadce2a2ad
Leo Cheung <lappangcheung@gmail.com>
2018-06-25 15:07:01 -0400

    footer landing navbar initial sets done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Landing from './components/layouts/Landing';
+
 class App extends Component{
     render(){
         return(
             <div className="App">
-                <h1>My React App</h1>
+                <Navbar />
+                <Landing />
+                <Footer />
             </div>
         )
     }
