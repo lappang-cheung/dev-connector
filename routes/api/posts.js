45a5837976b4ff6fc0441410d4f0e538c5931d9b
Leo Cheung <lappangcheung@gmail.com>
2018-06-25 00:07:47 -0400

    remove and add like post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -121,4 +121,36 @@ router.post('/like/:id', passport.authenticate('jwt', {session: false}), (req,re
         })
 });
 
+/*
+ * @route   POST api/posts/unlike/:id
+ * @desc    Unlike posts
+ * @access  Private
+ */
+router.post('/unlike/:id', passport.authenticate('jwt', {session: false}), (req,res) => {
+    Profile.findOne({ user: req.user.id})
+        .then(profile => {
+            Post.findById(req.params.id)
+            .then(post => {
+                if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+                    return res.status(400).json({
+                        notlike: 'You have no yet like this post'
+                    })
+                }
+
+                // Get remove index
+                const removeIndex = post.likes
+                    .map(item => item.user.toString())
+                    .indexOf(req.user.id);
+                // Splice out of array
+                post.likes.splice(removeIndex,1);
+
+                // save
+                post.save().then(post => res.json(post));
+            })
+            .catch(err => res.status(404).json({
+                postnotfound: 'No post found'
+            }))
+        })
+});
+
 module.exports = router;
