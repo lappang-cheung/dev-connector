4876932011990eb428d8ae0e2b69db31ca80bd2c
Leo Cheung <lappangcheung@gmail.com>
2018-06-25 00:03:18 -0400

    able to like post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -95,4 +95,30 @@ router.delete('/:id', passport.authenticate('jwt', {session: false}), (req,res) 
         })
 });
 
+/*
+ * @route   POST api/posts/like/:id
+ * @desc    Like posts
+ * @access  Private
+ */
+router.post('/like/:id', passport.authenticate('jwt', {session: false}), (req,res) => {
+    Profile.findOne({ user: req.user.id})
+        .then(profile => {
+            Post.findById(req.params.id)
+            .then(post => {
+                if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+                    return res.status(400).json({
+                        alreadylike: 'User already like this post'
+                    })
+                }
+
+                // Add user id to the like array
+                post.likes.unshift({ user: req.user.id});
+                post.save().then(post => res.json(post));
+            })
+            .catch(err => res.status(404).json({
+                postnotfound: 'No post found'
+            }))
+        })
+});
+
 module.exports = router;
