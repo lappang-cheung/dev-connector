b130035153cca28bd4f5f283bbd5397171f5e906
Leo Cheung <lappangcheung@gmail.com>
2018-06-25 18:25:30 -0400

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
@@ -8,22 +9,27 @@ import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import store from './store';
+
 import './App.css';
+
 
 class App extends Component{
     render(){
         return(
-            <Router>
-                <div className="App">
-                    <Navbar />
-                    <Route exact path='/' component={Landing} />
-                    <div className="container">
-                        <Route exact path='/register' component={Register} />
-                        <Route exact path='/login' component={Login} />
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Navbar />
+                        <Route exact path='/' component={Landing} />
+                        <div className="container">
+                            <Route exact path='/register' component={Register} />
+                            <Route exact path='/login' component={Login} />
+                        </div>
+                        <Footer />
                     </div>
-                    <Footer />
-                </div>
-            </Router>
+                </Router>
+            </Provider>
         )
     }
 }
