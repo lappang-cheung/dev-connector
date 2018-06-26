b0e940915266fd79c91b23b7807da69e22754696
lemonjai <lappangcheung@gmail.com>
2018-06-26 14:38:31 -0400

    components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 import { loginUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
    
@@ -57,22 +58,26 @@ class Login extends Component {
                         Sign in to your DevConnector account
                     </p>
                     <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                        <input
-                            type="email"
-                            className={classnames('form-control form-control-lg', {
-                            'is-invalid': errors.email
-                            })}
+                        
+                        <TextFieldGroup 
                             placeholder="Email Address"
                             name="email"
+                            type="email"
                             value={this.state.email}
                             onChange={this.onChange}
+                            error={errors.email}
                         />
-                        {errors.email && (
-                            <div className="invalid-feedback">{errors.email}</div>
-                        )}
-                        </div>
-                        <div className="form-group">
+
+                        <TextFieldGroup 
+                            placeholder="Password"
+                            name="password"
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.onChange}
+                            error={errors.password}
+                        />
+
+                        {/* <div className="form-group">
                         <input
                             type="password"
                             className={classnames('form-control form-control-lg', {
@@ -86,7 +91,7 @@ class Login extends Component {
                         {errors.password && (
                             <div className="invalid-feedback">{errors.password}</div>
                         )}
-                        </div>
+                        </div> */}
                         <input type="submit" className="btn btn-info btn-block mt-4" />
                     </form>
                     </div>
