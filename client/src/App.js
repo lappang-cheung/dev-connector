6033b0f294199301b45e213148db3177a80228d8
Leo Cheung <lappangcheung@gmail.com>
2018-06-25 22:37:09 -0400

    working on logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
 import { Provider } from 'react-redux';
 
 import Navbar from './components/layouts/Navbar';
@@ -9,9 +10,22 @@ import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
 import store from './store';
 
 import './App.css';
+
+// Check for token
+if(localStorage.jwtToken){
+    // Set auth token header auth
+    setAuthToken(localStorage.jwtToken);
+    // Decode token and get user info and expiry
+    const decoded = jwt_decode(localStorage.jwtToken);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+}
 
 
 class App extends Component{
